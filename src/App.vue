38be2e57c9a614b67d21ac3ab33c993adbe4d4a0
darneees <dgs56301@gmail.com>
2023-12-07 20:34:16 -0300

    adiconando sass ao projeto

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,35 @@
 <script setup>
-
-import { ref , computed } from "vue";
+import { ref, computed } from "vue";
 
 import "./assets/main.css";
 
-let id = 0
+let id = 0;
 
-const title = ref("title")
-const text = ref("text")
-const header = ref('header')
-const main = ref('main')
-const addTask = ref('addTask')
-const textTodo = ref('textTodo')
-const hidden = ref('hidden')
+const title = ref("title");
+const text = ref("text");
+const header = ref("header");
+const main = ref("main");
+const addTask = ref("addTask");
+const textTodo = ref("textTodo");
+const hidden = ref("hidden");
+const reserved = ref("reserved");
 
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([])
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const todos = ref([]);
 
 const filteredTodos = computed(() => {
-  return hideCompleted.value
-  ? todos.value.filter((t) => !t.done)
-  : todos.value
-})
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
+});
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
+  todos.value.push({ id: id++, text: newTodo.value, done: false });
+  newTodo.value = "";
 }
 
 function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
+  todos.value = todos.value.filter((t) => t !== todo);
 }
-
 </script>
 
 <template>
@@ -47,23 +44,27 @@ function removeTodo(todo) {
 
     <section :id="main">
       <form @submit.prevent="addTodo">
-        <input v-model="newTodo" :class="textTodo" placeholder="Write a task here">
+        <input
+          v-model="newTodo"
+          :class="textTodo"
+          placeholder="Write a task here"
+        />
         <button :class="addTask">+</button>
       </form>
 
       <ul>
-
         <li v-for="todo in filteredTodos" :key="todo.id">
-          <input type="checkbox" v-model="todo.done">
+          <input type="checkbox" v-model="todo.done" />
           <span :class="{ done: todo.done }">{{ todo.text }}</span>
           <button @click="removeTodo(todo)">X</button>
         </li>
-
       </ul>
 
       <button @click="hideCompleted = !hideCompleted" :class="hidden">
-        {{ hideCompleted ? 'Show' : 'Hidden' }}
+        {{ hideCompleted ? "Show" : "Hidden" }}
       </button>
     </section>
+
+    <span :class="reserved"><strong>© Douglas Sá</strong>. All rigths reserved.</span>
   </main>
 </template>
